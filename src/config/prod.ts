b465ad2e2a9d807907807c8891d70b0d48a53523
mmazt <mazetto.milton@gmail.com>
2018-05-23 22:45:25 -0300

    Alterações de estilo

--- src/config/prod.ts
+++ src/config/prod.ts
@@ -2,7 +2,7 @@ const environment = {
   api: 'https://api.spotify.com/v1',
   client_id: '9104aad8d41f437db269f56c5d4586ca',
   name: 'dev',
-  url: 'https://spotify-search-app.aerobaticapp.com/callback'
+  url: 'https://miltonmazetto-spotifyapp.surge.sh/callback'
 };
 
 export default environment;
